ee2923e46ae22cb7a77653a4cfb3cc1ca967aa33
Aidan James Rutter <arutter1@hydra24.eecs.utk.edu>
2018-10-01 15:00:09 -0400

    Added last7 hash function, comments

--- lab4/hash140.cpp
+++ lab4/hash140.cpp
@@ -1,5 +1,5 @@
 #include "hash140.h"
-
+#include <sstream>
 /**
  * Author: Aiden Rutter
  * CS140 Lab4: Hashing Stuff
@@ -8,12 +8,40 @@
 
 HashTable::HashTable(int table_size, string function, string collision)
 {
+    Fxn = function == "XOR";
+    Coll = collision == "Double";
+    keys.resize(table_size);
+    vals.resize(table_size);
+      
+}
 
+int XOR(string &key)
+{
+    // splits
+}
+
+int Last7(string &key)
+{
+    string last7;
+    int h;
+    if(key.size() > 7)
+    {
+        last7 = key.substr(key.size() - 7, 7);
+    }
+    else
+    {
+        last7 = key;
+    }
+    istringstream sin(last7);
+    sin >> hex >> h;
+    return h;
 }
 
 void HashTable::Add_Hash(string &key, string &val)
 {
-
+    // Last7: treats last 7 digits of hash string as a number in hexadecimal.
+    // if fewer than 7, treat last 7 as hex.
+      
 }
 
 string HashTable::Find(string &key)
--- lab4/hash140.h
+++ lab4/hash140.h
@@ -5,17 +5,26 @@ using namespace std;
 
 class HashTable {
   public:
+    // function can be either "Last7" or "XOR". collision can be either
+    // "Linear" or "Double"
     HashTable(int table_size, string function, string collision);
+    // Adds given key/value pair to hash table, assuming key is free
+    // Errors if: no more room in hash table, or key can't go into table
     void Add_Hash(string &key, string &val);
+    // Finds key and returns value
     string Find(string &key);
+    // Prints all non-empty slots in the hash table, one per line, index,
+    // right justified, and padded to five characters. Key & value, 
+    // separated by a space
     void Print();
+    // Calculates how may probes it takes to find for each key in the table.
     int Total_Probes();
   protected:
     vector <string> keys;
-    vector <string> vals;
-    int nkeys;
-    int Fxn;
-    int Coll;
+    vector <string> vals; 
+    int nkeys; // number of keys in the hash table
+    int Fxn;   // which hash function is being used
+    int Coll;  
     int tmp;
 };
 
