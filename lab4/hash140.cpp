047b54b7b2b5d53f6cab24ebec25bd6b632f3963
Aidan James Rutter <arutter1@hydra24.eecs.utk.edu>
2018-10-01 16:42:45 -0400

    added add_hash

--- lab4/hash140.cpp
+++ lab4/hash140.cpp
@@ -12,7 +12,8 @@ HashTable::HashTable(int table_size, string function, string collision)
     Coll = collision == "Double";
     keys.resize(table_size);
     vals.resize(table_size);
-      
+    tmp=0;
+    nkeys=0;
 }
 
 int XOR(string &key)
@@ -28,7 +29,7 @@ int XOR(string &key)
     hkeys.push_back(key.substr(i, key.size() - i));
     sin.str(hkeys[0]);
     sin >> hex >> hk;
-    for(int j=1; j<hkeys.size(); j++)
+    for(unsigned int j=1; j<hkeys.size(); j++)
     {
         sin.str(hkeys[i]);
         sin >> hex >> k;
@@ -56,9 +57,42 @@ int Last7(string &key)
 
 void HashTable::Add_Hash(string &key, string &val)
 {
-    // Last7: treats last 7 digits of hash string as a number in hexadecimal.
-    // if fewer than 7, treat last 7 as hex.
-      
+    int hi, hk, ts = keys.size(), i=0, h1, h2;
+    if(nkeys >= ts) cerr << "Hash Table Full" << endl;
+    if(Coll) // double hashing
+    {
+        // order of hash functions
+        h1 = Fxn ? XOR(key) : Last7(key);
+        h2 = Fxn ? Last7(key) : XOR(key);
+        // bad double hashing check
+        if(h2 % ts == 0)
+        {
+            cerr << "Couldn't put " << key << " into the table" << endl;
+        }
+        while(true)
+        {
+            tmp++;
+            hk = (h1 + i*h2) % ts;
+            if(keys[hk].empty()) break;
+            i++;
+        }
+        hi = hk;
+        
+    }
+    else // Linear hashing
+    {
+        hk = Fxn ? XOR(key) : Last7(key);
+        while(true)
+        {
+            tmp++; // for total_probes
+            if(keys[hk % ts].empty()) break;
+            hk++;
+        }
+        hi = hk % ts;
+    }
+    keys[hi] = key;
+    vals[hi] = val;
+    nkeys++;
 }
 
 string HashTable::Find(string &key)
