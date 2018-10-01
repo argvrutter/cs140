06d055c857081163c244afa619b06618eeba42f4
Aidan James Rutter <arutter1@hydra24.eecs.utk.edu>
2018-10-01 13:47:33 -0400

    Start of lab4

---
+++ lab4/hash140.cpp
@@ -0,0 +1,33 @@
+#include "hash140.h"
+
+/**
+ * Author: Aiden Rutter
+ * CS140 Lab4: Hashing Stuff
+ * Description: Implement methods in hash140.h
+ */
+
+HashTable::HashTable(int table_size, string function, string collision)
+{
+
+}
+
+void HashTable::Add_Hash(string &key, string &val)
+{
+
+}
+
+string HashTable::Find(string &key)
+{
+
+}
+
+void HashTable::Print()
+{
+
+}
+
+int HashTable::Total_Probes()
+{
+
+}
+
