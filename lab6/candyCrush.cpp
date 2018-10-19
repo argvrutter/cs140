32d8cc9fbff328d9d465ba80f1eb3ef110d7185b
Aidan James Rutter <arutter1@hydra24.eecs.utk.edu>
2018-10-19 09:31:54 -0400

    Spooky Skeltal code for lab6

---
+++ lab6/candyCrush.cpp
@@ -0,0 +1,26 @@
+/**
+ * Author: Aiden Rutter
+ * CS140 Lab6 CandyCrush
+ * Description: Implement candyCrush.h
+ */
+#include "candyCrush.h"
+
+candyCrush::candyCrush(string inputFile)
+{
+
+}
+
+int candyCrush::getRowLength()
+{
+
+}
+
+void candyCrush::printCandy()
+{
+
+}
+
+int candyCrush::play(int choice)
+{
+    
+}
