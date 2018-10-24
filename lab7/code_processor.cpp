5adb9eb1e6e57fea129204fcc59b9f33a0f1d855
Aidan James Rutter <arutter1@hydra30.eecs.utk.edu>
2018-10-24 15:09:05 -0400

    Spooky skeltal code for lab 7

---
+++ lab7/code_processor.cpp
@@ -0,0 +1,133 @@
+/**
+ * Author: Aiden Rutter
+ * CS140 Lab 7: Code processing
+ * Description: Implement code processor.h
+ * Notes: 2 week lab, partial submission date:
+ * Partial cases: 1-3, 6, 10-13, 15, 18, 20, 21
+ * 23, 26, 29-31
+ */
+/**
+ * [Code_Processor::New_Prize description]
+ * @param  id          [description]
+ * @param  description [description]
+ * @param  points      [description]
+ * @param  quantity    [description]
+ * @return             [description]
+ */
+int Code_Processor::New_Prize(string id, string description, int points, int quantity)
+{
+
+}
+/**
+ * [Code_Processor::New_User description]
+ * @param  username        [description]
+ * @param  realname        [description]
+ * @param  starting_points [description]
+ * @return                 [description]
+ */
+int Code_Processor::New_User(string username, string realname, int starting_points)
+{
+
+}
+/**
+ * [Code_Processor::Delete_User description]
+ * @param  username [description]
+ * @return          [description]
+ */
+int Code_Processor::Delete_User(string username)
+{
+
+}
+/**
+ * [Code_Processor::Add_Phone description]
+ * @param  username [description]
+ * @param  phone    [description]
+ * @return          [description]
+ */
+int Code_Processor::Add_Phone(string username, string phone)
+{
+
+}
+/**
+ * [Code_Processor::Remove_Phone description]
+ * @param  username [description]
+ * @param  phone    [description]
+ * @return          [description]
+ */
+int Code_Processor::Remove_Phone(string username, string phone)
+{
+
+}
+/**
+ * [Code_Processor::Show_Phones description]
+ * @param  username [description]
+ * @return          [description]
+ */
+string Code_Processor::Show_Phones(string username)
+{
+
+}
+/**
+ * [Code_Processor::Enter_Code description]
+ * @param  username [description]
+ * @param  code     [description]
+ * @return          [description]
+ */
+int Code_Processor::Enter_Code(string username, string code)
+{
+
+}
+/**
+ * [Code_Processor::Text_Code description]
+ * @param  phone [description]
+ * @param  code  [description]
+ * @return       [description]
+ */
+int Code_Processor::Text_Code(string phone, string code)
+{
+
+}
+/**
+ * [Code_Processor::Mark_Code_Used description]
+ * @param  code [description]
+ * @return      [description]
+ */
+int Code_Processor::Mark_Code_Used(string code)
+{
+
+}
+/**
+ * [Code_Processor::Balance description]
+ * @param  username [description]
+ * @return          [description]
+ */
+int Code_Processor::Balance(string username)
+{
+
+}
+/**
+ * [Code_Processor::Redeem_Prize description]
+ * @param  username [description]
+ * @param  prize    [description]
+ * @return          [description]
+ */
+int Code_Processor::Redeem_Prize(string username, string prize)
+{
+
+}
+/**
+ * [Code_Processor description]
+ */
+Code_Processor::~Code_Processor()
+{
+
+}
+/**
+ * [Code_Processor::Write description]
+ * @param  file [description]
+ * @return      [description]
+ */
+int Code_Processor::Write(const char *file)
+{
+
+}
